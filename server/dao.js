7b0d1c64d59ed83c52cf22b4a9bc6ac3337dc869
Dore Chen <dore_chen@hotmail.com>
2018-04-15 16:06:17 +1200

    Add post API for adding an entry

--- server/dao.js
+++ server/dao.js
@@ -1,11 +1,17 @@
 // Define Database Queries
 const db = require('../db')
-const getEntries = (currentUser) => {
-  return db.query('SELECT * FROM entries where userid=$1', [currentUser])
+const insertEntry = (user, event, lastoccurence, frequency) => {
+  return db.query('INSERT INTO entries VALUES (DEFAULT, $1, $2, $3, $4)', [user, event, lastoccurence, frequency])
+    .then(res => res)
+    .catch(e=> console.error(e.stack))
+}
+const getEntries = (user) => {
+  return db.query('SELECT * FROM entries where userid=$1', [user])
     .then(res => res.rows)
     .catch(e => console.error(e.stack))
 }
 
 module.exports = {
   getEntries,
+  insertEntry,
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const { getEntries, insertEntry } = require('./dao.js')
+
 const app = express()
-const { getEntries } = require('./dao.js')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('/entries/:id: Gets entries for given id'))
 
 app.get('/entries/:id(\\d+)', async (req, res) => {
   const entries = await getEntries(req.params.id)
   res.send(entries)
+})
+
+app.post('/entry/', async (req, res) => {
+  const body = req.body
+  const entry = await insertEntry(body.user, body.event, body.lastoccurence, body.frequency)
+  res.send(entry);
 })
 
 app.listen(3000, () => console.log('Hisabisa listening on port 3000'))
