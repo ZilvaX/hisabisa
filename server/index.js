a7b22e0b361f4d51cd82c98a49e7b6b82885a6c2
Dore Chen <dore_chen@hotmail.com>
2018-04-17 23:23:41 +1200

    Add ESLint configurations

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.get('/entries/:id(\\d+)', async (req, res) => {
 app.post('/entry/', async (req, res) => {
   const body = req.body
   const entry = await insertEntry(body.user, body.event, body.lastoccurence, body.frequency)
-  res.send(entry);
+  res.send(entry)
 })
 
 app.listen(3000, () => console.log('Hisabisa listening on port 3000'))
