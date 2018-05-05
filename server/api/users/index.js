e5fe677d264f74e020e27160f26c9c7326d48d23
Kevin <kevin.x.g@gmail.com>
2018-05-05 17:38:55 +1200

    Add API to create users
    Added argon to has password when storing.
    Also made usernames unique.

---
+++ server/api/users/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const { argon2i } = require('argon2-ffi')
+const crypto = require('crypto')
+const { promisify } = require('util')
+
+const { insertUser } = require('../../dao.js')
+
+const randomBytes = promisify(crypto.randomBytes)
+
+router.post('/', (req, res) => {
+  const body = req.body
+  randomBytes(32)
+    .then(salt => argon2i.hash(body.password, salt))
+    .then(async hashedPassword => {
+      const result = await insertUser(body.username, hashedPassword)
+      if (result) {
+        res.status(200)
+        res.send(result)
+      } else {
+        res.status(400)
+        res.send('DBMS Error')
+      }
+    })
+})
+
+module.exports = router
--- server/dao.js
+++ server/dao.js
@@ -1,17 +1,32 @@
 // Define Database Queries
 const db = require('./db')
 const insertEntry = (user, event, lastoccurence, frequency) => {
-  return db.query('INSERT INTO entries VALUES (DEFAULT, $1, $2, $3, $4)', [user, event, lastoccurence, frequency])
+  return db
+    .query('INSERT INTO entries VALUES (DEFAULT, $1, $2, $3, $4)', [
+      user,
+      event,
+      lastoccurence,
+      frequency,
+    ])
     .then(res => res)
-    .catch(e=> console.error(e.stack))
+    .catch(e => console.error(e.stack))
 }
-const getEntries = (user) => {
-  return db.query('SELECT * FROM entries where userid=$1', [user])
+const getEntries = user => {
+  return db
+    .query('SELECT * FROM entries where userid=$1', [user])
     .then(res => res.rows)
+    .catch(e => console.error(e.stack))
+}
+
+const insertUser = (user, hashedPassword) => {
+  return db
+    .query('INSERT INTO users VALUES (DEFAULT, $1, $2)', [user, hashedPassword])
+    .then(res => res)
     .catch(e => console.error(e.stack))
 }
 
 module.exports = {
   getEntries,
   insertEntry,
+  insertUser,
 }
