c9935d609c2d0cce5dfe50c8885de8c15985851e
Kevin <kevin.x.g@gmail.com>
2018-05-13 15:33:31 +1200

    Wrap db calls with try catch blocks

--- server/api/entries/index.js
+++ server/api/entries/index.js
@@ -3,24 +3,36 @@ const { getEntries, insertEntry, getOverdueEntries } = require('../../dao.js')
 const router = express.Router()
 
 router.get('/:id(\\d+)', async (req, res) => {
-  const entries = await getEntries(req.params.id)
-  res.send(entries)
+  try {
+    const entries = await getEntries(req.params.id)
+    res.send(entries)
+  } catch (e) {
+    res.status(500).send('Failed to obtain entries')
+  }
 })
 
 router.get('/:id(\\d+)/overdue', async (req, res) => {
-  const entries = await getOverdueEntries(req.params.id)
-  res.send(entries)
+  try {
+    const entries = await getOverdueEntries(req.params.id)
+    res.send(entries)
+  } catch (e) {
+    res.status(500).send('Failed to obtain overdue entries')
+  }
 })
 
 router.post('/', async (req, res) => {
-  const body = req.body
-  const entry = await insertEntry(
-    body.user,
-    body.event,
-    body.lastoccurence,
-    body.frequency,
-  )
-  res.send(entry)
+  try {
+    const body = req.body
+    const entry = await insertEntry(
+      body.user,
+      body.event,
+      body.lastoccurence,
+      body.frequency,
+    )
+    res.status(201).send(entry)
+  } catch (e) {
+    res.status(500).send('Failed to store entry')
+  }
 })
 
 module.exports = router
