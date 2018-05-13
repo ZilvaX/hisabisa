3e475591528e9424b99284dd39240bca2349dd29
Kevin <kevin.x.g@gmail.com>
2018-05-13 15:32:04 +1200

    Remove error handling on dao

--- server/dao.js
+++ server/dao.js
@@ -9,13 +9,11 @@ const insertEntry = (user, event, lastoccurence, frequency) => {
       frequency,
     ])
     .then(res => res)
-    .catch(e => console.error(e.stack))
 }
 const getEntries = user => {
   return db
     .query('SELECT * FROM entries WHERE userid=$1', [user])
     .then(res => res.rows)
-    .catch(e => console.error(e.stack))
 }
 
 const getOverdueEntries = user => {
@@ -25,14 +23,12 @@ const getOverdueEntries = user => {
       [user],
     )
     .then(res => res.rows)
-    .catch(e => console.error(e.stack))
 }
 
 const insertUser = (user, hashedPassword) => {
   return db
     .query('INSERT INTO users VALUES (DEFAULT, $1, $2)', [user, hashedPassword])
     .then(res => res)
-    .catch(e => console.error(e.stack))
 }
 
 module.exports = {
