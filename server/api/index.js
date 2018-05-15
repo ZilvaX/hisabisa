d788a4969c4232683554c2e22a3d44174b104432
Dore Chen <dore_chen@hotmail.com>
2018-05-15 22:55:46 +1200

    Implement JSON web token authentication api

--- server/api/index.js
+++ server/api/index.js
@@ -3,16 +3,39 @@ const router = express.Router()
 const entries = require('./entries')
 const users = require('./users')
 const authentication = require('./authentication')
+const jwt = require('jsonwebtoken')
+const { secret } = require('../config.js')
+const { promisify } = require('util')
 
 // define the home page route
 router.get('/', function(req, res) {
   res.send('API home page \n /entries/:id: Gets entries for given id')
 })
 
-router.use('/entries', entries)
-
-router.use('/users', users)
-
 router.use('/authentication', authentication)
+
+router.use((req, res, next) => {
+  const token = req.headers.authorization
+  const verify = promisify(jwt.verify)
+  if (token) {
+    verify(token, secret)
+      .then(result => {
+        if (result) {
+          next()
+        }
+      })
+      .catch(e => {
+        res.status(401)
+        res.send('Token verification failed')
+        console.error(e)
+      })
+  } else {
+    res.status(401)
+    res.send('No token provided')
+  }
+})
+
+router.use('/entries', entries)
+router.use('/users', users)
 
 module.exports = router
