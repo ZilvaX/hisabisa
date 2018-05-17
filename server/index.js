9f4b737ef47e991118539d27f0fab11b0453e967
Kevin <kevin.x.g@gmail.com>
2018-05-17 22:02:04 +1200

    Use helmet for some sane security defaults

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = require('./api')
+const helmet = require('helmet')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(helmet())
+
 app.use('/api', router)
 
 app.listen(3000, () => console.log('Hisabisa listening on port 3000'))
