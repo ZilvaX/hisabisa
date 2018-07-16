d138c662b607815a92c7a56453c78191904f708d
Kevin <kevin.x.g@gmail.com>
2018-07-16 21:57:58 +1200

    Declare SnackbarContainer as a function
    Also add prop types

--- client/components/SnackbarContainer.js
+++ client/components/SnackbarContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import Snackbar from '@material-ui/core/Snackbar'
@@ -7,37 +8,41 @@ import CloseIcon from '@material-ui/icons/Close'
 
 import { hideError } from '../actions'
 
-class SnackbarContainer extends React.Component {
-  render() {
-    const onClose = () => {
-      this.props.dispatch(hideError())
-    }
-    return (
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        open={this.props.showSnackbar}
-        autoHideDuration={4000}
-        onClose={onClose}
-        ContentProps={{
-          'aria-describedby': 'message-id',
-        }}
-        message={<span id="message-id">{this.props.errorMessage}</span>}
-        action={[
-          <IconButton
-            key="close"
-            aria-label="Close"
-            color="inherit"
-            onClick={onClose}
-          >
-            <CloseIcon />
-          </IconButton>,
-        ]}
-      />
-    )
+function SnackbarContainer(props) {
+  const onClose = () => {
+    props.dispatch(hideError())
   }
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      open={props.showSnackbar}
+      autoHideDuration={4000}
+      onClose={onClose}
+      ContentProps={{
+        'aria-describedby': 'message-id',
+      }}
+      message={<span id="message-id">{props.errorMessage}</span>}
+      action={[
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>,
+      ]}
+    />
+  )
+}
+
+SnackbarContainer.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  showSnackbar: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string,
 }
 const mapStateToProps = state => ({
   showSnackbar: state.errors.showSnackbar,
