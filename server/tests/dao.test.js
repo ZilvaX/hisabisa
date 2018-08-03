f6728b9c58e99419eb890b9176548ff9967bb338
Kevin <kevin.x.g@gmail.com>
2018-08-03 23:54:44 +1200

    Extract type configuration to helper method

--- server/tests/dao.test.js
+++ server/tests/dao.test.js
@@ -1,20 +1,14 @@
-const pgInterval = require('postgres-interval')
 const { Client, types } = require('pg')
 const { Duration, DateTime } = require('luxon')
+const typeConfigurer = require('../db/typeConfigurer')
 const db = require('../db')
 const dao = require('../dao')
 jest.mock('../db')
 
 // Type Parsers
-const INTERVAL_OID = 1186
-types.setTypeParser(INTERVAL_OID, value =>
-  Duration.fromObject(pgInterval(value)),
-)
-const DATE_OID = 1082
-types.setTypeParser(DATE_OID, value => DateTime.fromISO(value))
+typeConfigurer(types)
 
 let client
-beforeAll(() => {})
 beforeEach(() => {
   // Create new client for each test
   client = new Client({
