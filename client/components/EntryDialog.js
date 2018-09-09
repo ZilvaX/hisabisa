da5c43b865b556e5dbc1a131a9a4fc582b173721
Dore Chen <dore_chen@hotmail.com>
2018-09-09 21:46:11 +1200

    Fix a typo

--- client/components/EntryDialog.js
+++ client/components/EntryDialog.js
@@ -48,7 +48,7 @@ function EntryDialog(props) {
           <Button onClick={props.handleClose} color="primary">
             Cancel
           </Button>
-          <Button type="sumbit" color="primary">
+          <Button type="submit" color="primary">
             {props.submitbutton}
           </Button>
         </DialogActions>
