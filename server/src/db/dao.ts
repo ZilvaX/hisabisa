7464d545cf3f86528d5cd5b14f0e2ee2b940330e
Kevin <kevin.x.g@gmail.com>
2018-10-03 23:05:02 +1300

    Use SQL query builders to build queries

--- server/src/db/dao.ts
+++ server/src/db/dao.ts
@@ -1,8 +1,23 @@
 import db from './connection'
+const sql = require('sql')
+
+// Define tables
+const users = sql.define({
+  name: 'users',
+  columns: ['userid', 'username', 'password'],
+})
+
+// Dao functions
 function checkUserExists(username) {
-  return db
-    .query('select userid from users where username = $1', [username])
-    .then(res => res.rows.length > 0)
+  const query = sql
+    .select(
+      users
+        .subQuery()
+        .where(users.username.equals(username))
+        .exists(),
+    )
+    .toQuery()
+  return db.query(query.text, query.values).then(res => res.rows[0].exists)
 }
 
 export default { checkUserExists }
